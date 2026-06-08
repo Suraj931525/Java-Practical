27bb3dccd9263f9213931945e7e722089e686388
Suraj931525 <surajpatil9315@gmail.com>
2026-06-08 11:47:09 +0530

    Add task in day3

---
+++ day3/comparetwonumbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class comparetwonumbers{
+ public static void main(String[]args){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter First Number");
+int number1=sc.nextInt();
+
+System.out.println("Enter second Number");
+int number2=sc.nextInt();
+
+if(number1>number2){
+
+System.out.println(number1+" is greatest number" );
+}
+else{
+System.out.println(number2+" is greatest number" );
+}
+}
+}
